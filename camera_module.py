4b8179dbcafe8995f5cb9f7be011af2b209059d9
Eugene Galaxy <jgalak16@student.aau.dk>
2020-03-20 23:19:34 +0100

    Added realsense(some problem with frame wait timeout). Fixed webcam img

--- camera_module.py
+++ camera_module.py
@@ -1,9 +1,11 @@
 import cv2
 import pyrealsense2 as rs
 import numpy as np
+import matplotlib.pyplot as plt  # Plotting tool
+import time
 
 
-def get_webcam_image():
+def get_webcam_image(save=None, plot=None):
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
@@ -13,68 +15,92 @@ def get_webcam_image():
     r = 250.0 / frame.shape[1]
     dim = (250, int(frame.shape[0] * r))
     resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-    cv2.imwrite('new_entries/person/image.jpg', resized)
     cap.release()
-    cv2.destroyAllWindows()
-    return resized
+    if save is not None:
+        cv2.imwrite('new_entries/person/image.jpg', resized)
+    if plot is not None:
+        cv2.namedWindow('Webcam Photo', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Webcam Photo', resized)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    return resized[..., ::-1]
 
 
-# Create a pipeline
-pipeline = rs.pipeline()
+def video_feed_TEST():
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-#  Create a config and configure the pipeline to stream
-#  different resolutions of color and depth streams
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # Start streaming
+    pipeline.start(config)
 
-profile = pipeline.start(config)
+    try:
+        while True:
 
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
-print("Depth Scale is: ", depth_scale)
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
 
-# We will be removing the background of objects more than
-#  clipping_distance_in_meters meters away
-clipping_distance_in_meters = 1  # 1 meter
-clipping_distance = clipping_distance_in_meters / depth_scale
-align_to = rs.stream.color
-align = rs.align(align_to)
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
 
-try:
-    while True:
-        # Get frameset of color and depth
-        frames = pipeline.wait_for_frames()
-        # frames.get_depth_frame() is a 640x360 depth image
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-        # Align the depth frame to color frame
-        aligned_frames = align.process(frames)
+            # Stack both images horizontally
+            images = np.hstack((color_image, depth_colormap))
 
-        # Get aligned frames
-        aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
-        color_frame = aligned_frames.get_color_frame()
+            # Show images
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            cv2.waitKey(1)
 
-        # Validate that both frames are valid
-        if not aligned_depth_frame or not color_frame:
-            continue
+    finally:
 
-        depth_image = np.asanyarray(aligned_depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+        # Stop streaming
+        pipeline.stop()
 
-        # Remove background - Set pixels further than clipping_distance to grey
-        grey_color = 153
-        depth_image_3d = np.dstack((depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
-        bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
 
-        # Render images
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        images = np.hstack((bg_removed, depth_colormap))
-        cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('Align Example', images)
-        key = cv2.waitKey(1)
-        # Press esc or 'q' to close the image window
-        if key & 0xFF == ord('q') or key == 27:
-            cv2.destroyAllWindows()
-            break
-finally:
-    pipeline.stop()
+def rs_init_camera():
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    pipeline.start(config)
+
+    for x in range(5):
+        pipeline.wait_for_frames()
+    return pipeline
+
+
+def rs_capture_frame(pipeline):
+    frames = pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
+    color_image = np.asanyarray(color_frame.get_data())
+    return color_image
+
+
+def getImage(save=None, plot=None):
+    pipe = rs_init_camera()
+    image = rs_capture_frame(pipe)
+    pipe.stop()
+    if save is not None:
+        cv2.imwrite('new_entries/person/image.jpg', image)
+    if plot is not None:
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    return image[..., ::-1]
+
+
+def load_image(path):
+    img = cv2.imread(path, 1)
+    return img[..., ::-1]  # Reversing from BGR to RGB
+
+
--- face_verification.py
+++ face_verification.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt  # Plotting tool
 from align import AlignDlib  # Face alignment method
 from model import create_model  # CNN library
 from load_dataset import load_metadata, find_language_code  # Loading training set in from folder
-from camera_module import get_webcam_image
+from camera_module import *
 
 
 class FaceVerification(object):
@@ -13,21 +13,30 @@ class FaceVerification(object):
     weights_pt = 'weights/nn4.small2.v1.h5'
     database_pt = 'target_database'
     newtarget_pt = 'new_entries'
-    threshold = 0.65
+    threshold = 0.75
 
     def __init__(self):
         self.nn4_small2_pretrained = create_model()  # Create a Neural network model
         self.nn4_small2_pretrained.load_weights(self.weights_pt)  # Use pre-trained weights
         self.alig_model = AlignDlib(self.landmarks_pt)  # Initialize the OpenFace face alignment utility
-        self.init_database()
+        self.init_database(target_names=1)
+    #     self.pipeline = rs_init_camera()
+
+    # def __del__(self):
+    #     if self.pipeline in locals():
+    #         self.pipeline.stop()
 
     # ---------------------------------- PUBLIC METHODS ----------------------------------
     def doPrediction(self, plot=None):
-        self.target_metadata = load_metadata(self.newtarget_pt)
-        self.target_features = self.extract_features(self.target_metadata)
+        # self.target_metadata = load_metadata(self.newtarget_pt)
+        # self.target_features = self.extract_features(self.target_metadata)
+
+        fresh_image = get_webcam_image(save=1)
+        # fresh_image = load_image('new_entries/person/image.jpg')
+        self.target_features = self.test_extract_feature(fresh_image)
 
         all_dist, min_dist, min_idx = self.dist_target_to_database()
-
+        print(all_dist)
         target_recognised = self.threshold_check(min_dist)
 
         if target_recognised is True:
@@ -36,11 +45,11 @@ class FaceVerification(object):
             print("Target recognized as " + str(target_name) + ". Language: " + str(lang_str))
 
             if plot is not None:
-                plt.figure(num="Face Verification", figsize=(8, 4))
+                plt.figure(num="Face Verification", figsize=(8, 5))
                 plt.suptitle("Most similar to {0} with Distance of {1:1.3f}\n".format(target_name, min_dist)
                              + "Language code '{0}': {1}.".format(lang_code, lang_str))
                 plt.subplot(121)
-                plt.imshow(self.load_image(self.target_metadata[0].image_path()))
+                plt.imshow(fresh_image)
                 plt.subplot(122)
                 plt.imshow(self.load_image(self.database_metadata[min_idx].image_path()))
                 plt.show()
@@ -49,9 +58,17 @@ class FaceVerification(object):
 
     #  ---------------------------------- PRIVATE METHODS ----------------------------------
 
-    def init_database(self):
-        self.database_metadata = load_metadata(self.database_pt, names=1)
+    def init_database(self, target_names=None):
+        self.database_metadata = load_metadata(self.database_pt, names=target_names)
         self.database_features = self.extract_features(self.database_metadata)
+
+    def test_extract_feature(self, img):
+        embedded = np.zeros(128)
+        time.sleep(1)
+        img = self.align_image(img)
+        img = (img / 255.).astype(np.float32)  # scale RGB values to interval [0,1]
+        embedded = self.nn4_small2_pretrained.predict(np.expand_dims(img, axis=0))[0]
+        return embedded
 
     def extract_features(self, metadata):
         embedded = np.zeros((metadata.shape[0], 128))
@@ -66,7 +83,7 @@ class FaceVerification(object):
         distances = []  # squared L2 distance between pairs
         num = len(self.database_metadata)
         for i in range(num):
-            distances.append(self.distance(self.database_features[i], self.target_features[0]))
+            distances.append(self.distance(self.database_features[i], self.target_features))
         distances = np.array(distances)
 
         min_dist = np.amin(distances)
@@ -101,3 +118,45 @@ class FaceVerification(object):
 
 FV = FaceVerification()
 FV.doPrediction(plot=1)
+
+# img1 = get_webcam_image(save=1)  # perevernutaja
+# print('img1 Dimensions :', img1.shape)
+# img1_emb = FV.test_extract_feature(img1)
+# time.sleep(5)
+
+# img2 = cv2.imread('new_entries/person/image.jpg', 1)  # ne perevernutaja
+# print('img2 Dimensions :', img2.shape)
+# img2_emb = FV.test_extract_feature(img2)
+
+# img3 = img2[..., ::-1]  # perevernutaja
+# print('img3 Dimensions :', img3.shape)
+# img3_emb = FV.test_extract_feature(img3)
+
+# test_img = cv2.imread('target_database/Jevgenijs_Galaktionovs-ru/ae.jpg', 1)
+# print('test_img Dimensions :', test_img.shape)
+# test_img_emb = FV.test_extract_feature(test_img)
+
+# dist_img1_to_img2 = FV.distance(img1_emb, img2_emb)
+# dist_img1_to_img3 = FV.distance(img1_emb, img3_emb)
+# dist_img2_to_img3 = FV.distance(img2_emb, img3_emb)
+# dist_img3_to_img3 = FV.distance(img3_emb, img3_emb)
+
+# print('Distance img1 to img2: {0:1.4f}'.format(dist_img1_to_img2))
+# print('Distance img1 to img3: {0:1.4f}'.format(dist_img1_to_img3))
+# print('Distance img2 to img3: {0:1.4f}'.format(dist_img2_to_img3))
+# print('Distance img3 to img3: {0:1.4f}'.format(dist_img3_to_img3))
+# print('==========================================================')
+
+# dist_img1_to_test = FV.distance(test_img_emb, img1_emb)
+# dist_img2_to_test = FV.distance(test_img_emb, img2_emb)
+# dist_img3_to_test = FV.distance(test_img_emb, img3_emb)
+# print('Distance test to img1: {0:1.4f}'.format(dist_img1_to_test))
+# print('Distance test to img2: {0:1.4f}'.format(dist_img2_to_test))
+# print('Distance test to img3: {0:1.4f}'.format(dist_img3_to_test))
+
+# plt.figure(num="TEST FV", figsize=(8, 5))
+# plt.subplot(121)
+# plt.imshow(img1)
+# plt.subplot(122)
+# plt.imshow(img3)
+# plt.show()
